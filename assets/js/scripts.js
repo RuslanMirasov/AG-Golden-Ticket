3e6c1db65b4ad20c33a01f166b02b0e9957ff589
Ruslan <info@mirasov.dev>
2026-01-19 16:43:46 +0100

    hero and prizes sections responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 import { initNavigationMenu, fixHeaderOnScroll } from './helpers.js';
 import { initScrollToBlock } from './scrollToBlock.js';
+import { initSliders } from './sliders.js';
 
 initNavigationMenu();
 fixHeaderOnScroll();
 initScrollToBlock();
+initSliders();
